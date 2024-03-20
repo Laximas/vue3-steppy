066d33d5537134ea5c29705754354df6f116f425
Laximas <mkonstan@hotmail.com>
2024-03-20 21:51:36 +0200

    Add type definitions

---
+++ src/index.d.ts
@@ -0,0 +1,21 @@
+declare module 'vue3-steppy' {
+  import { DefineComponent } from 'vue';
+
+  export interface SteppyProps {
+    step: number;
+    tabs: Array<{ title: string; iconSuccess?: string; isValid: boolean; }>;
+    finalize: () => void;
+    backText: string;
+    nextText: string;
+    doneText: string;
+    loading: boolean;
+    primaryColor1: string;
+    primaryColor2: string;
+    backgroundColor: string;
+    circleSize: number;
+  }
+
+  const Steppy: DefineComponent<SteppyProps>;
+
+  export default Steppy;
+}
