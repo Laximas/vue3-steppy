c816c7cd7d42ebac2ea18e18d8e2f031a42a46fe
Laximas <mkonstan@hotmail.com>
2026-03-09 19:52:26 +0100

    Updates

---
+++ vue3-steppy.d.ts
@@ -0,0 +1,27 @@
+declare module "@/components/Vue3Steppy.vue" {
+  import { DefineComponent } from "vue";
+
+  export interface SteppyProps {
+    step: number;
+    tabs: Array<{
+      title: string;
+      description?: string;
+      iconSuccess?: string;
+      isValid: boolean;
+    }>;
+    finalize: () => void;
+    backText: string;
+    nextText: string;
+    doneText: string;
+    loading: boolean;
+    primaryColor1: string;
+    primaryColor2: string;
+    backgroundColor: string;
+    circleSize: number;
+    contentMarginTop: number;
+  }
+
+  const Steppy: DefineComponent<SteppyProps>;
+
+  export default Steppy;
+}
